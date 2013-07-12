f36fb1f1751935c454af62dc4e7b309e872e5315
Max Jacobson <maxwell.jacobson@gmail.com>
2013-07-12 10:07:46 -0400

    organized code a little

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,12 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, :through => :recipe_ingredients
 
   def ingredients_to_add=(ingredients_names)
-    ingredients_names.collect(&:strip).collect(&:downcase).reject(&:empty?).uniq.each do |ingredient_name|
+    ingredients_names
+    .collect(&:strip)
+    .collect(&:downcase)
+    .reject(&:empty?)
+    .uniq
+    .each do |ingredient_name|
       ingreed = Ingredient.where(name: ingredient_name).first_or_create
       self.recipe_ingredients.build(ingredient: ingreed)
     end
